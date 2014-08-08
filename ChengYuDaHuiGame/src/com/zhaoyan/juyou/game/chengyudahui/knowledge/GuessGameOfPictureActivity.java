1c0a68ced409ed626fe59a6fccf35d152572d90a
yuri <xlog2013@163.com>
2014-08-08 15:02:16 +0800

    fix a error bug

--- ChengYuDaHuiGame/src/com/zhaoyan/juyou/game/chengyudahui/knowledge/GuessGameOfPictureActivity.java
+++ ChengYuDaHuiGame/src/com/zhaoyan/juyou/game/chengyudahui/knowledge/GuessGameOfPictureActivity.java
@@ -25,7 +25,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.baidu.kirin.d.d;
 import com.zhaoyan.communication.util.Log;
 import com.zhaoyan.juyou.game.chengyudahui.R;
 import com.zhaoyan.juyou.game.chengyudahui.db.WordData.WordColums;
@@ -37,8 +36,10 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 	private static final String TAG = GuessGameOfPictureActivity.class.getSimpleName();
 	
 	private GridView mGridView;
-	private MyAdapter mAdapter;
+	private PicGuessAdapter mAdapter;
 	private ImageView mTipView, mFreeView;
+	private ImageView mBackView;
+	
 	private AnswerButton[] mAnswerBtns = new AnswerButton[4];
 	
 	private List<Word> mWordsList = new ArrayList<Word>();
@@ -54,9 +55,18 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 		String path = getIntent().getStringExtra("path");
 		Log.d(TAG, "path:" + path);
 		
+		initView();
+		
+		//init 
+		initAnswerButtonArray();
+	}
+	
+	private void initView(){
+		mBackView = (ImageView) findViewById(R.id.iv_back);
 		mTipView = (ImageView) findViewById(R.id.iv_cy_tip);
 		mFreeView = (ImageView) findViewById(R.id.iv_cy_free);
 		
+		mBackView.setOnClickListener(this);
 		mTipView.setOnClickListener(this);
 		mFreeView.setOnClickListener(this);
 		
@@ -73,14 +83,9 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 		mWordsList = getData(testStr);
 
 		mGridView = (GridView) findViewById(R.id.gv_cy_word);
-		mAdapter = new MyAdapter(getApplicationContext());
+		mAdapter = new PicGuessAdapter(getApplicationContext());
 		mGridView.setAdapter(mAdapter);
 		mGridView.setOnItemClickListener(this);
-		
-		//init 
-		for (int i = 0; i < 4; i++) {
-			mAnswerArray.put(i, false);
-		}
 	}
 	
 	/**
@@ -94,10 +99,11 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 		Cursor cursor = getContentResolver().query(
 				WordColums.CONTENT_URI, new String[] {"word"}, null,
 				null, null);
-		Log.d(TAG, "cursor.count=" + cursor.getCount());
+		int count = cursor.getCount();
+		Log.d(TAG, "cursor.count=" + count);
 		
 		//生成20个不重复的随机数
-		Set<Integer> set = Utils.getRandomNums(20, 4766);
+		Set<Integer> set = Utils.getRandomNums(20, count);
 		String letter = "";
 		Iterator<Integer> iterator = set.iterator();
 		while (iterator.hasNext()) {
@@ -118,7 +124,7 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 		List<Word> wordList = new ArrayList<Word>();
 		Word word = null;
 		for(String string : list){
-			//得到每个字所在的位置
+			//得到重新排序后每个字所在的位置
 			int position = list.indexOf(string);
 			word = new Word();
 			word.setPosition(position);
@@ -127,14 +133,160 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 		}
 		return wordList;
 	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Log.d(TAG, "onItemClick");
+		if (mAnswerArray.indexOfValue(false) == -1 ) {
+			//四个答案格斗填满了，而且都是错的，那么再来的话 就重新开始吧
+			//清楚掉当前答案框的内容
+			clearAnswerButton();
+		}
+		
+		Word word2 = mWordsList.get(position);
+		
+		//当用户点击grdiview中的字的时候，依次从第一个开始判断哪个框是空着的，就将用户选中的字放上去
+		for (int i = 0; i < 4; i++) {
+			if (!mAnswerArray.get(i)) {//
+				mAnswerArray.put(i, true);//置为true，表示我这个框有字了
+				mAnswerBtns[i].setWord(word2);
+				mAdapter.setVisibile(position, false);//点击过了，就在gridview中隐藏掉
+				mAdapter.notifyDataSetChanged();
+				
+				if (mAnswerArray.indexOfValue(false) == -1 ) {
+					//用户已经找到四个字了
+					String answer = "";
+					for(Button button : mAnswerBtns){
+						//将四个字组成一个词，看一下是不是我们的答案
+						answer += button.getText();
+					}
+					if (answer.equals(testStr)) {
+						//答对了，弹出对话框，显示该成语意思
+						showRightDialog();
+						setButtonsColor(Color.GREEN);
+					} else {
+						//打错了，将字体颜色改为红色，以提醒用户
+						setButtonsColor(Color.RED);
+					}
+				}
+				return;
+			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.iv_back:
+			GuessGameOfPictureActivity.this.finish();
+			break;
+		case R.id.iv_cy_tip:
+			break;
+		case R.id.iv_cy_free:
+			break;
+		case R.id.btn_answer_01:
+			clickAnswerButton(1);
+			break;
+		case R.id.btn_answer_02:
+			clickAnswerButton(2);
+			break;
+		case R.id.btn_answer_03:
+			clickAnswerButton(3);
+			break;
+		case R.id.btn_answer_04:
+			clickAnswerButton(4);
+			break;
+
+		default:
+			break;
+		}
+	}
 	
-	class MyAdapter extends BaseAdapter{
+	private void initAnswerButtonArray(){
+		for (int i = 0; i < 4; i++) {
+			mAnswerArray.put(i, false);
+		}
+	}
+	
+	/**
+	 * 设置答案框内文字的颜色，正常时黑色，答对是绿色，答错是红色
+	 * @param color
+	 */
+	private void setButtonsColor(int color){
+		for(Button button : mAnswerBtns){
+			button.setTextColor(color);
+		}
+	}
+	
+	/**
+	 * @param n 成语答案中的第几个答案框 1,2,3,4
+	 */
+	private void clickAnswerButton(int n){
+		Log.d(TAG, "clickAnswerButton:" + n);
+		//切换到数组下标
+		n = n - 1;
+		
+		if (!mAnswerArray.get(n)) {
+			//答案框内已经是空的话，就点击无效了
+			return;
+		}
+
+		setButtonsColor(Color.BLACK);
+		
+		Word word = mAnswerBtns[n].getWord();
+		mAdapter.setVisibile(word.getPosition(), true);
+		mAdapter.notifyDataSetChanged();
+		
+		mAnswerBtns[n].setWord(null);
+		mAnswerArray.put(n, false);
+	}
+	
+	private void clearAnswerButton(){
+		Log.d(TAG, "clearAnswerButton");
+		setButtonsColor(Color.BLACK);
+		
+		//4 代表四个答案框
+		Word word = null;
+		for (int i = 0; i < 4; i++) {
+			word = mAnswerBtns[i].getWord();
+			mAdapter.setVisibile(word.getPosition(), true);
+			mAnswerBtns[i].setWord(null);
+			mAnswerArray.put(i, false);
+		}
+		mAdapter.notifyDataSetChanged();
+	}
+	
+	private void showRightDialog(){
+		final NiftyDialogBuilder dialogBuilder = NiftyDialogBuilder.getInstance(this);
+		dialogBuilder.withTitle(testStr)
+		.withTitleColor("#000000")
+		.withDividerColor("#11000000")
+		.withMessage("成语解释在这里.")
+		.withMessageColor("#000000")
+		.withIcon(getResources().getDrawable(R.drawable.ic_launcher))
+		.isCancelableOnTouchOutside(false) 
+		.withDuration(450)
+		.withEffect(Effectstype.SlideBottom) 
+		.withButton1Text("下一题") 
+		.setCustomView(R.layout.custom_view, getApplicationContext()) 
+		.setButton1Click(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialogBuilder.dismiss();
+				Toast.makeText(v.getContext(), "NExt",
+						Toast.LENGTH_SHORT).show();
+			}
+		}).show();
+	}
+	
+	class PicGuessAdapter extends BaseAdapter{
 		private LayoutInflater mInflater = null;
 		
+		//用于标示Gridview中的item隐藏还是显示
 		private SparseBooleanArray mVisibleArray;
 		
-		public MyAdapter(Context context) {
-			// TODO Auto-generated constructor stub
+		public PicGuessAdapter(Context context) {
 			mInflater = LayoutInflater.from(context);
 			mVisibleArray = new SparseBooleanArray(mWordsList.size());
 			for (int i = 0; i < mWordsList.size(); i++) {
@@ -176,154 +328,6 @@ public class GuessGameOfPictureActivity extends Activity implements OnItemClickL
 			return view;
 		}
 		
-	}
-
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,
-			long id) {
-		Log.d(TAG, "onItemClick");
-		Word word2 = mWordsList.get(position);
-		
-		if (mAnswerArray.indexOfValue(false) == -1 ) {
-			//四个答案格斗填满了，而且都是错的，那么再来的话 就重新开始吧
-			Word word = null;
-			for (int i = 0; i < 4; i++) {
-//				word = mWordsList.get(mAnswerBtns[i].getWord().getPosition());
-//				word.setVisibile(true);
-				mAdapter.setVisibile(mAnswerBtns[i].getWord().getPosition(), true);
-				mAnswerArray.put(i, false);
-				mAnswerBtns[i].setWord(null);
-			}
-			setButtonsColor(Color.BLACK);
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			if (!mAnswerArray.get(i)) {
-				mAnswerArray.put(i, true);
-				mAnswerBtns[i].setWord(word2);
-				mAdapter.setVisibile(position, false);
-				mAdapter.notifyDataSetChanged();
-				
-				if (mAnswerArray.indexOfValue(false) == -1 ) {
-					String answer = "";
-					for(Button button : mAnswerBtns){
-						answer += button.getText();
-					}
-					if (answer.equals(testStr)) {
-						//答对了，弹出对话框，显示该成语意思
-						showRightDialog();
-						setButtonsColor(Color.GREEN);
-					} else {
-						setButtonsColor(Color.RED);
-					}
-				}
-				return;
-			}
-		}
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.iv_cy_tip:
-			break;
-		case R.id.iv_cy_free:
-			break;
-		case R.id.btn_answer_01:
-			setButtonsColor(Color.BLACK);
-			
-			Word word = mAnswerBtns[0].getWord();
-			mAdapter.setVisibile(word.getPosition(), true);
-			mAdapter.notifyDataSetChanged();
-			
-			mAnswerBtns[0].setWord(null);
-			mAnswerArray.put(0, false);
-			break;
-		case R.id.btn_answer_02:
-			setButtonsColor(Color.BLACK);
-			
-			Word word1 = mAnswerBtns[1].getWord();
-			mAdapter.setVisibile(word1.getPosition(), true);
-			mAdapter.notifyDataSetChanged();
-			
-			mAnswerBtns[1].setWord(null);
-			mAnswerArray.put(1, false);
-			break;
-		case R.id.btn_answer_03:
-			setButtonsColor(Color.BLACK);
-			
-			Word word2 = mAnswerBtns[2].getWord();
-			mAdapter.setVisibile(word2.getPosition(), true);
-			mAdapter.notifyDataSetChanged();
-			
-			mAnswerBtns[2].setWord(null);
-			mAnswerArray.put(2, false);
-			break;
-		case R.id.btn_answer_04:
-			setButtonsColor(Color.BLACK);
-			
-			Word word3 = mAnswerBtns[3].getWord();
-			mAdapter.setVisibile(word3.getPosition(), true);
-			mAdapter.notifyDataSetChanged();
-			
-			mAnswerBtns[3].setWord(null);
-			mAnswerArray.put(3, false);
-			break;
-
-		default:
-			break;
-		}
-	}
-	
-	private void setButtonsColor(int color){
-		for(Button button : mAnswerBtns){
-			button.setTextColor(color);
-		}
-	}
-	
-	/**
-	 * @param n 
-	 */
-	private void clickAnswerButton(int n){
-		setButtonsColor(Color.BLACK);
-		
-		Word word = mAnswerBtns[n].getWord();
-		mAdapter.setVisibile(word.getPosition(), true);
-		mAdapter.notifyDataSetChanged();
-		
-		mAnswerBtns[n].setWord(null);
-		mAnswerArray.put(n, false);
-	}
-	
-	private void showRightDialog(){
-		final NiftyDialogBuilder dialogBuilder = NiftyDialogBuilder.getInstance(this);
-		dialogBuilder.withTitle(testStr)
-		// .withTitle(null) no title
-		.withTitleColor("#000000")
-		// def
-		.withDividerColor("#11000000")
-		// def
-		.withMessage("成语解释在这里.")
-		// .withMessage(null) no Msg
-		.withMessageColor("#000000")
-		// def
-		.withIcon(getResources().getDrawable(R.drawable.ic_launcher))
-		.isCancelableOnTouchOutside(true) // def | isCancelable(true)
-		.withDuration(700) // def
-		.withEffect(Effectstype.SlideBottom) // def Effectstype.Slidetop
-		.withButton1Text("下一题") // def gone
-		.setCustomView(R.layout.custom_view, getApplicationContext()) // .setCustomView(View
-																// ResId,context)
-		.setButton1Click(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dialogBuilder.dismiss();
-				Toast.makeText(v.getContext(), "NExt",
-						Toast.LENGTH_SHORT).show();
-			}
-		});
-		dialogBuilder.setCancelable(false);
-		dialogBuilder.show();
 	}
 	
 }
