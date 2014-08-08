c087182411fecf8d04f860827998465c6a361b05
liangxiaofei <liangxiaofei1100@163.com>
2014-08-08 11:19:12 +0800

    update chengyu dictionary

--- ChengYuDaHuiGame/src/com/zhaoyan/juyou/game/chengyudahui/activity/ChengYuDictionary.java
+++ ChengYuDaHuiGame/src/com/zhaoyan/juyou/game/chengyudahui/activity/ChengYuDictionary.java
@@ -12,12 +12,17 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,17 +31,17 @@ import com.zhaoyan.juyou.game.chengyudahui.ChengYu;
 import com.zhaoyan.juyou.game.chengyudahui.R;
 import com.zhaoyan.juyou.game.chengyudahui.db.ChengyuData.ChengyuColums;
 
-public class ChengYuDictionary extends Activity {
+public class ChengYuDictionary extends Activity implements OnItemClickListener {
 	private static final String TAG = ChengYuDictionary.class.getSimpleName();
 	private static final int CHENGYU_TOTAL_NUMBER = 29349;
 
 	private Context mContext;
-	private EditText mSearchEditText;
+	private AutoCompleteTextView mSearchEditText;
 	private View mClearTextView;
 
 	private ViewPager mViewPager;
 	private PagerAdapter mPagerAdapter;
-	private List<ChengYu> mChengYus;
+	private List<ChengYu> mChengYuList;
 	private int mCurrentViewPageIndex = 0;
 
 	private TextView mChengYuNameTextView;
@@ -46,61 +51,94 @@ public class ChengYuDictionary extends Activity {
 	private ImageView mNextImageView;
 
 	private ChengyuQuery mChengyuQuery;
-	private static final int TOKEN_INIT_QUERY = 1;
+	private static final int TOKEN_SINGLE_QUERY = 1;
 	private static final int TOKEN_HEAD_QUERY = 2;
 	private static final int TOKEN_TAIL_QUERY = 3;
+	private static final int TOKEN_SEARCHBOX_QUERY = 4;
 
 	private static final String[] PROJECTION = { ChengyuColums._ID,
 			ChengyuColums.NAME, ChengyuColums.PINYIN, ChengyuColums.COMMENT,
 			ChengyuColums.ORIGINAL, ChengyuColums.EXAMPLE };
 
-	private int mCurrentChengYuId = 100;
+	private int mInitChengYuId = 100;
+	private int mCurrentChengYuId = mInitChengYuId;
 	private int mPageHeadChengYuId = mCurrentChengYuId;
 	private int mPageTailChengYuId = mCurrentChengYuId;
 	private static final int PRELOAD_NUMBER_HEAD = 10;
 	private static final int PRELOAD_NUMBER_TAIL = 10;
+
+	private SimpleCursorAdapter mSearchListAdapter;
+	private Cursor mSearchCursor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chengyu_dictionary);
 		mContext = this;
-
 		mChengyuQuery = new ChengyuQuery(getContentResolver());
+		mChengYuList = new ArrayList<ChengYu>();
 
 		initView();
+
+		movetoChengYu(mInitChengYuId);
 		updatePrievousAndNextButton();
-
-		mChengYus = new ArrayList<ChengYu>();
-		mPagerAdapter = new ChengYuViewPagerAdapter();
-		mViewPager.setAdapter(mPagerAdapter);
-		mViewPager.setOnPageChangeListener(new ChengYuPagerChangeListener());
-
-		initChengYu(mCurrentChengYuId);
 	}
 
-	private void initChengYu(int id) {
-		Log.d(TAG, "initChengYu id = " + id);
-		String selection = ChengyuColums._ID + ">="
-				+ (id - PRELOAD_NUMBER_TAIL) + " and " + ChengyuColums._ID
-				+ "<=" + (id + PRELOAD_NUMBER_HEAD);
-		mChengyuQuery.startQuery(TOKEN_INIT_QUERY, null,
-				ChengyuColums.CONTENT_URI, PROJECTION, selection, null, null);
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// release search cursor
+		Cursor cursor = mSearchListAdapter.swapCursor(null);
+		if (cursor != null && !cursor.isClosed()) {
+			cursor.close();
+		}
 	}
 
 	private void initView() {
-		mSearchEditText = (EditText) findViewById(R.id.et_search);
+		// Search views
+		mSearchEditText = (AutoCompleteTextView) findViewById(R.id.et_search);
 		mSearchEditText.addTextChangedListener(new SearchTextWatcher());
-
+		mSearchListAdapter = new SearchAdapter(mContext);
+		mSearchEditText.setAdapter(mSearchListAdapter);
+		mSearchEditText.setOnItemClickListener(this);
 		mClearTextView = findViewById(R.id.iv_clear_text);
+		mSearchEditText.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View arg0) {
+				String selection = ChengyuColums.NAME + " like '"
+						+ mSearchEditText.getText().toString() + "%'";
+				mChengyuQuery.startQuery(TOKEN_SEARCHBOX_QUERY, null,
+						ChengyuColums.CONTENT_URI, PROJECTION, selection, null,
+						null);
+			}
+		});
+		// ChengYu views
 		mChengYuNameTextView = (TextView) findViewById(R.id.tv_chengyu_name);
 		mChengYuIndexTextView = (TextView) findViewById(R.id.tv_chengyu_index);
-
 		mPreviousImageView = (ImageView) findViewById(R.id.iv_previous);
 		mNextImageView = (ImageView) findViewById(R.id.iv_next);
-
 		mViewPager = (ViewPager) findViewById(R.id.vp_chengyu);
+		mPagerAdapter = new ChengYuViewPagerAdapter();
+		mViewPager.setAdapter(mPagerAdapter);
+		mViewPager.setOnPageChangeListener(new ChengYuPagerChangeListener());
+	}
+
+	/**
+	 * Move to chengyu with the id.
+	 * 
+	 * @param id
+	 */
+	private void movetoChengYu(int id) {
+		Log.d(TAG, "movetoChengYu id = " + id);
+		mCurrentChengYuId = id;
+		mPageHeadChengYuId = mCurrentChengYuId;
+		mPageTailChengYuId = mCurrentChengYuId;
+		String selection = ChengyuColums._ID + ">="
+				+ (id - PRELOAD_NUMBER_TAIL) + " and " + ChengyuColums._ID
+				+ "<=" + (id + PRELOAD_NUMBER_HEAD);
+		mChengyuQuery.startQuery(TOKEN_SINGLE_QUERY, null,
+				ChengyuColums.CONTENT_URI, PROJECTION, selection, null, null);
 	}
 
 	private void updatePrievousAndNextButton() {
@@ -158,7 +196,7 @@ public class ChengYuDictionary extends Activity {
 	}
 
 	private void loadNextPage() {
-		if (mCurrentViewPageIndex == mChengYus.size() - 2) {
+		if (mCurrentViewPageIndex == mChengYuList.size() - 2) {
 			Log.d(TAG, "loadNextPage");
 			if (mPageHeadChengYuId >= CHENGYU_TOTAL_NUMBER) {
 				// The last data.
@@ -201,6 +239,12 @@ public class ChengYuDictionary extends Activity {
 				mClearTextView.setVisibility(View.GONE);
 			} else {
 				mClearTextView.setVisibility(View.VISIBLE);
+
+				String selection = ChengyuColums.NAME + " like '"
+						+ s.toString() + "%'";
+				mChengyuQuery.startQuery(TOKEN_SEARCHBOX_QUERY, null,
+						ChengyuColums.CONTENT_URI, PROJECTION, selection, null,
+						null);
 			}
 		}
 
@@ -222,7 +266,7 @@ public class ChengYuDictionary extends Activity {
 
 		@Override
 		public int getCount() {
-			return mChengYus.size();
+			return mChengYuList.size();
 		}
 
 		@Override
@@ -232,7 +276,7 @@ public class ChengYuDictionary extends Activity {
 
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
-			ChengYu chengYu = mChengYus.get(position);
+			ChengYu chengYu = mChengYuList.get(position);
 
 			View view = LayoutInflater.from(mContext).inflate(
 					R.layout.chengyu_content, null);
@@ -258,6 +302,11 @@ public class ChengYuDictionary extends Activity {
 		public void destroyItem(ViewGroup container, int position, Object object) {
 			container.removeView((View) object);
 		}
+
+		@Override
+		public int getItemPosition(Object object) {
+			return POSITION_NONE;
+		}
 	}
 
 	class ChengYuPagerChangeListener implements OnPageChangeListener {
@@ -275,10 +324,10 @@ public class ChengYuDictionary extends Activity {
 		@Override
 		public void onPageSelected(int position) {
 			mCurrentViewPageIndex = position;
-			mCurrentChengYuId = mChengYus.get(position).id;
-			mChengYuNameTextView.setText(mChengYus.get(position).name);
-			mChengYuIndexTextView.setText("第" + mCurrentChengYuId + "个，共"
-					+ CHENGYU_TOTAL_NUMBER + "个");
+			mCurrentChengYuId = mChengYuList.get(position).id;
+
+			updateChengYuNameAndId(mChengYuList.get(position).name,
+					mCurrentChengYuId);
 			updatePrievousAndNextButton();
 
 			loadNextPage();
@@ -287,6 +336,16 @@ public class ChengYuDictionary extends Activity {
 
 	}
 
+	private void updateChengYuNameAndId(String name, int id) {
+		mChengYuNameTextView.setText(name);
+		mChengYuIndexTextView.setText("第" + id + "个，共" + CHENGYU_TOTAL_NUMBER
+				+ "个");
+	}
+
+	/**
+	 * Query ChengYu database.
+	 * 
+	 */
 	private class ChengyuQuery extends AsyncQueryHandler {
 
 		public ChengyuQuery(ContentResolver cr) {
@@ -298,8 +357,8 @@ public class ChengYuDictionary extends Activity {
 			super.onQueryComplete(token, cookie, cursor);
 			Log.d(TAG, "onQueryComplete token = " + token);
 			switch (token) {
-			case TOKEN_INIT_QUERY:
-				initQuery(cursor);
+			case TOKEN_SINGLE_QUERY:
+				singleQuery(cursor);
 				break;
 			case TOKEN_HEAD_QUERY:
 				headQuery(cursor);
@@ -307,10 +366,30 @@ public class ChengYuDictionary extends Activity {
 			case TOKEN_TAIL_QUERY:
 				tailQuery(cursor);
 				break;
+			case TOKEN_SEARCHBOX_QUERY:
+				searchQuery(cursor);
+				break;
 			default:
 				break;
 			}
 
+		}
+
+		private void searchQuery(Cursor cursor) {
+			mSearchCursor = cursor;
+			Cursor old = mSearchListAdapter.swapCursor(mSearchCursor);
+			if (old != null && !old.isClosed()) {
+				old.close();
+			}
+			mSearchListAdapter.notifyDataSetChanged();
+
+			if (cursor != null && cursor.getCount() > 0) {
+				mSearchEditText.setCompletionHint("共搜索到" + cursor.getCount()
+						+ "个成语");
+				mSearchCursor.moveToFirst();
+				ChengYu chengYu = getChengYu(mSearchCursor);
+				Log.d(TAG, "chengyu " + chengYu.name + " id = " + chengYu.id);
+			}
 		}
 
 		private void tailQuery(Cursor cursor) {
@@ -356,22 +435,28 @@ public class ChengYuDictionary extends Activity {
 			}
 		}
 
-		private void initQuery(Cursor cursor) {
+		private void singleQuery(Cursor cursor) {
 			if (cursor != null) {
 				try {
+					// Clear the list.
+					mChengYuList.clear();
+					// Add the query result into list.
 					while (cursor.moveToNext()) {
 						ChengYu chengYu = getChengYu(cursor);
 
 						int id = cursor.getInt(cursor
 								.getColumnIndex(ChengyuColums._ID));
 
+						// Update head and tail id.
 						if (id == mCurrentChengYuId) {
-							mChengYuNameTextView.setText(chengYu.name);
-						} else if (id < mCurrentChengYuId - 1) {
+							updateChengYuNameAndId(chengYu.name, id);
+						} else if (id < mCurrentChengYuId) {
+							// update tail id if needed
 							if (id < mPageTailChengYuId) {
 								mPageTailChengYuId = id;
 							}
 						} else if (id > mCurrentChengYuId) {
+							// update head id if needed
 							if (id > mPageHeadChengYuId) {
 								mPageHeadChengYuId = id;
 							}
@@ -379,13 +464,18 @@ public class ChengYuDictionary extends Activity {
 						addChengYuIntoViewPager(chengYu);
 					}
 					mPagerAdapter.notifyDataSetChanged();
+					// Update current ChengYu
+					Log.d(TAG, "tail id = " + mPageTailChengYuId
+							+ ", currentId = " + mCurrentChengYuId
+							+ ", headId = " + mPageHeadChengYuId);
+					Log.d(TAG, "chengyu list " + mChengYuList.toString());
 					if (mCurrentChengYuId != mPageTailChengYuId) {
 						mCurrentViewPageIndex = mCurrentChengYuId
 								- mPageTailChengYuId;
 						mViewPager.setCurrentItem(mCurrentViewPageIndex, false);
 					}
 				} catch (Exception e) {
-					Log.e(TAG, "initQuery " + e);
+					Log.e(TAG, "singleQuery " + e);
 				} finally {
 					cursor.close();
 				}
@@ -416,10 +506,49 @@ public class ChengYuDictionary extends Activity {
 
 		private void addChengYuIntoViewPager(ChengYu chengYu, int position) {
 			if (position == -1) {
-				mChengYus.add(chengYu);
+				mChengYuList.add(chengYu);
 			} else {
-				mChengYus.add(position, chengYu);
+				mChengYuList.add(position, chengYu);
 			}
 		}
 	}
+
+	private class SearchAdapter extends SimpleCursorAdapter {
+
+		public SearchAdapter(Context context) {
+			super(context, android.R.layout.simple_list_item_1, mSearchCursor,
+					new String[] { ChengyuColums.NAME },
+					new int[] { android.R.id.text1 },
+					SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+		}
+
+		@Override
+		public CharSequence convertToString(Cursor cursor) {
+			if (cursor != null) {
+				return cursor.getString(cursor
+						.getColumnIndex(ChengyuColums.NAME));
+			}
+			return "";
+		}
+	}
+
+	/**
+	 * Search list item click
+	 */
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		movetoChengYu((int) id);
+		closeInputMethod(mSearchEditText);
+	}
+
+	private void closeInputMethod(View view) {
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		boolean isOpen = imm.isActive();
+		if (isOpen) {
+			imm.hideSoftInputFromWindow(view.getWindowToken(),
+					InputMethodManager.HIDE_NOT_ALWAYS);
+		}
+	}
+
 }
