d202253282d685805a77d8acdbfd53ba147a9521
yuri <xlog2013@163.com>
2014-11-21 09:16:16 +0800

    add cycg

---
+++ ChengYuDaHuiGame/src/com/zhaoyan/juyou/game/chengyudahui/interation/CYCGMainActivity.java
@@ -0,0 +1,83 @@
+package com.zhaoyan.juyou.game.chengyudahui.interation;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.zhaoyan.juyou.game.chengyudahui.R;
+import com.zhaoyan.juyou.game.chengyudahui.activity.BaseZyActivity;
+import com.zhaoyan.juyou.game.chengyudahui.view.ActionBar;
+import com.zhaoyan.juyou.game.chengyudahui.view.LetterImageView;
+
+/**
+ * 成语闯关 主界面
+ * chengyu chuangguan mainui
+ * @author Yuri
+ *
+ */
+public class CYCGMainActivity extends BaseZyActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_write_main);
+		
+		ActionBar actionBar = getZyActionBar();
+		actionBar.setActionHomeAsUpEnable(true);
+		
+		Intent intent = getIntent();
+		if (intent != null) {
+			actionBar.setTitle(intent.getStringExtra("title"));
+		}
+		
+		ListView listView = (ListView) findViewById(R.id.lv_write);
+		String[] items = getResources().getStringArray(R.array.write_items);
+		SampleListAdapter adapter = new SampleListAdapter(this, Arrays.asList(items));
+		listView.setAdapter(adapter);
+	}
+	
+	private static class SampleListAdapter extends ArrayAdapter<String> {
+		private Context mContext;
+
+        public SampleListAdapter(Context context, List<String> objects) {
+            super(context, R.layout.activity_write_item, objects);
+            mContext = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String name = getItem(position);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_write_item, parent, false);
+            }
+
+            LetterImageView letterImageView = (LetterImageView) convertView.findViewById(R.id.liv_write_num);
+            letterImageView.setOval(true);
+            TextView textView = (TextView) convertView.findViewById(R.id.tv_item_title);
+            letterImageView.setLetter(((position + 1) + "").charAt(0));
+            textView.setText(name);
+
+            return convertView;
+        }
+        
+    }
+}
